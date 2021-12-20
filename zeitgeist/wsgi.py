c4a02a3175bfd42a50ba560e0d24ecfb42e778f7
Yash Mittal <23.yashm@gmail.com>
2021-12-20 20:01:06 +0530

    env file in wsgi

--- zeitgeist/wsgi.py
+++ zeitgeist/wsgi.py
@@ -8,8 +8,15 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+from dotenv import load_dotenv
+ENV_PATH = '../../../virtualenv/Z22/.env'
+load_dotenv(ENV_PATH)
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.insert(0, '/home3/zeitgeist/Z22/Zeitgeist22')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zeitgeist.settings')
 
